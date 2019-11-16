1d6926b9d6ce34ed2f8810c6d2f4994ce2db0eed
Jamie <jamie@forwardaction.uk>
2019-11-16 16:00:31 +0000

    setting up the registration page

--- client/src/components/Hello.vue
+++ client/src/components/Hello.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Hello',
   props: {
     msg: String
   }
---
+++ client/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Register from '@/components/Register'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    }
+  ]
+})
